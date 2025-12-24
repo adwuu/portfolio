ef17055def7dadf4d906bd5283874fa8b1ed806c
adwuu <adam@aminoanalytica.com>
2025-12-24 20:01:09 +0000

    Fix: remove references to deleted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Home } from "./pages/Home";
-import { ProjectDetail } from "./pages/ProjectDetail";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects/:slug" element={<ProjectDetail />} />
         </Routes>
       </Layout>
     </BrowserRouter>
--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,4 @@
 export { Hero } from "./Hero";
-export { About } from "./About";
 export { AminoAnalytica } from "./AminoAnalytica";
 export { Adwuu } from "./Adwuu";
 export { Film } from "./Film";
